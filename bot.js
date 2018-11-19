4c1d46f8b5957be7636191d676e8147b7f082c2a
Panz155 <41524573+Panz155@users.noreply.github.com>
2018-11-19 17:18:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
  client.on('message', message => {
               if(!message.channel.guild) return;
     var prefix = "$";
-    if(message.content.startsWith('$bc')) {
+    if(message.content.startsWith('*bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
@@ -71,3 +71,5 @@ if (message.content.startsWith(adminprefix + 'setT')) {
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
 });
+
+client.login(process.env.BOT_TOKEN);
